61f570cb9b784689908c15ca3571256e518eae35
hafizur-02 <hafizur.ra02@gmail.com>
2022-02-22 21:47:21 +0600

    add component

---
+++ src/components/NewComponent/NewComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NewComponent = () => {
+    return (
+        <div>
+            <>
+                <h2>Welcome to our New Component House</h2>
+            </>
+        </div>
+    );
+};
+
+export default NewComponent;
